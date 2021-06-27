d228b6828cef5d758a9d2429d2f39e9cb85c2fb3
CN <ckddnjs554@naver.com>
2021-06-27 23:47:06 +0900

    Feat : lambda 문법 테스트 코드

--- app/src/main/java/com/yoochangwons/todolist/LambdaTest.kt
+++ app/src/main/java/com/yoochangwons/todolist/LambdaTest.kt
@@ -2,7 +2,9 @@ package com.yoochangwons.todolist
 
 fun main(array: Array<String>) {
 
-    print(funs { sum(1,2) + 1 })
+    println(funs { sum(1,2) + 1 })
+    val test = {a: Int, b: Int -> a + b}
+    println(test(1,2))
 }
 
 private fun sum(a: Int, b: Int): Int {
