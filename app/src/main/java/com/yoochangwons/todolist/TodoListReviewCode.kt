a6320c77b82d74b13fc258f2f55d15eef9d0b27b
CN <ckddnjs554@naver.com>
2021-06-28 00:58:00 +0900

    Feat : TodoListReviewCode 레이아웃 작성

---
+++ app/src/main/java/com/yoochangwons/todolist/TodoListReviewCode.kt
@@ -0,0 +1,11 @@
+package com.yoochangwons.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TodoListReviewCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_list_review_code)
+    }
+}
