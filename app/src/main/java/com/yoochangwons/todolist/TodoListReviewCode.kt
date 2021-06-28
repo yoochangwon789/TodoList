d8598195e59d9fd9ce204cda7b242b7a3ebb6e1b
CN <ckddnjs554@naver.com>
2021-06-28 19:10:48 +0900

    Feat : TodoList review 코드 view binding 구현 작업 코드

--- app/src/main/java/com/yoochangwons/todolist/TodoListReviewCode.kt
+++ app/src/main/java/com/yoochangwons/todolist/TodoListReviewCode.kt
@@ -8,20 +8,26 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.yoochangwons.todolist.databinding.ActivityTodoListReviewCodeBinding
+import com.yoochangwons.todolist.databinding.ItemTodoBinding
+import com.yoochangwons.todolist.databinding.ItemTodoReviewBinding
 
 class TodoListReviewCode : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTodoListReviewCodeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todo_list_review_code)
+        binding = ActivityTodoListReviewCodeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val dataTodo = ArrayList<TodoListReview>()
         dataTodo.add(TodoListReview("숙제", false))
         dataTodo.add(TodoListReview("청소", false))
 
-        val adapter = TodoRecyclerViewReview(dataTodo)
-        val recyclerViewReview = findViewById<RecyclerView>(R.id.recycler_view_review)
-        recyclerViewReview.adapter = adapter
-        recyclerViewReview.layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewReview.adapter = TodoRecyclerViewReview(dataTodo)
+        binding.recyclerViewReview.layoutManager = LinearLayoutManager(this)
     }
 }
 
@@ -31,21 +37,16 @@ class TodoRecyclerViewReview(
     val dataList: List<TodoListReview>
 ) : RecyclerView.Adapter<TodoRecyclerViewReview.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView
-
-        init {
-            textView = itemView.findViewById(R.id.todo_text_review)
-        }
+    class ViewHolder(val binding: ItemTodoReviewBinding) : RecyclerView.ViewHolder(binding.root) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo_review, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(ItemTodoReviewBinding.bind(view))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = dataList[position].todo
+        holder.binding.todoTextReview.text = dataList[position].todo
     }
 
     override fun getItemCount(): Int {
