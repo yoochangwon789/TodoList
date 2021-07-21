7dba148857b28c5a8b258ecc13e81c5061633e6c
CN <ckddnjs554@naver.com>
2021-07-21 23:52:49 +0900

    Feat : also 구문 test 구문

--- app/src/main/java/com/yoochangwons/todolist/LambdaTest.kt
+++ app/src/main/java/com/yoochangwons/todolist/LambdaTest.kt
@@ -1,6 +1,7 @@
 package com.yoochangwons.todolist
 
 import java.util.*
+import kotlin.random.Random
 
 fun main(array: Array<String>) {
 
@@ -9,10 +10,14 @@ fun main(array: Array<String>) {
     println(test(1,2))
 
     val toUpperCase = {str: String -> str.uppercase() }
-    println(toUpperCase("str"))
+//    println(toUpperCase("str"))
 
     val strList = listOf<String>("a", "b", "c")
-    println(strList.map { s: String -> s.uppercase() })
+//    println(strList.map { s: String -> s.uppercase() })
+
+    Random.nextInt(100).also {
+        print("Random : $it")
+    }
 }
 
 private fun sum(a: Int, b: Int): Int {
