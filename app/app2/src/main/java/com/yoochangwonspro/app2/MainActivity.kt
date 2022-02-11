cd3d567b620258d8be380d37e5b36027c837acad
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-02-12 01:15:18 +0900

    Feat : ViewModel, TodoRepository view binding 프로퍼티 추가

--- app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.yoochangwonspro.app2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.yoochangwonspro.app2.databinding.ActivityMainBinding
+import com.yoochangwonspro.app2.model.repository.TodoRepository
+import com.yoochangwonspro.app2.viewmodel.TodoViewModel
+import com.yoochangwonspro.app2.viewmodel.TodoViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var todoViewModel: TodoViewModel
+
+    private lateinit var todoRepository: TodoRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        todoRepository = TodoRepository(application)
+        todoViewModel =
+            ViewModelProvider(this, TodoViewModelFactory(todoRepository))[TodoViewModel::class.java]
+
+        binding.addTodoButton.setOnClickListener {
+            startActivity(
+                Intent(this, AddTodoActivity::class.java)
+            )
+        }
     }
 }
