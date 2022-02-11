6990c1f2c501a9b9a107956433f72b0ee312b5ea
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-02-12 00:51:39 +0900

    Feat : TodoRepository class 추가 및 함수 구현

--- app/app2/src/main/java/com/yoochangwonspro/app2/model/repository/TodoRepository.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/model/repository/TodoRepository.kt
@@ -1,10 +1,19 @@
 package com.yoochangwonspro.app2.model.repository
 
 import android.app.Application
-import android.content.Context
+import androidx.lifecycle.LiveData
 import com.yoochangwonspro.app2.model.TodoDatabaseProvider
+import com.yoochangwonspro.app2.model.entity.Todo
 
 class TodoRepository(application: Application) {
 
     private val todoList = TodoDatabaseProvider.getTodoDatabase(application).todoListDao()
+
+    fun getAll(): LiveData<List<Todo>> {
+        return todoList.getAll()
+    }
+
+    fun insert(todo: Todo) {
+        todoList.insert(todo)
+    }
 }
