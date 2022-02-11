afb6e820c78473a451db98d62971caefd8ac0938
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-02-12 01:20:49 +0900

    Feat : adapter 프로퍼티 초기화 작업 및 recyclerView 초기 셋업

--- app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.yoochangwonspro.app2.adapter.TodoAdapter
 import com.yoochangwonspro.app2.databinding.ActivityMainBinding
 import com.yoochangwonspro.app2.model.repository.TodoRepository
 import com.yoochangwonspro.app2.viewmodel.TodoViewModel
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var todoRepository: TodoRepository
 
+    private lateinit var adapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,6 +29,14 @@ class MainActivity : AppCompatActivity() {
         todoRepository = TodoRepository(application)
         todoViewModel =
             ViewModelProvider(this, TodoViewModelFactory(todoRepository))[TodoViewModel::class.java]
+
+        adapter = TodoAdapter()
+        binding.todoRecyclerView.adapter = adapter
+        binding.todoRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        todoViewModel.getAll().observe(this, { todoList ->
+            adapter.setData(todoList)
+        })
 
         binding.addTodoButton.setOnClickListener {
             startActivity(
