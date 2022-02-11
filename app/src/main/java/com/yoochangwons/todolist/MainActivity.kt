7d045a7b27650a12725deb7168d54e6e0e8502e5
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-02-11 23:14:05 +0900

    Add : app2 module 추가

--- app/src/main/java/com/yoochangwons/todolist/MainActivity.kt
+++ app/src/main/java/com/yoochangwons/todolist/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -18,7 +17,6 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.yoochangwons.todolist.databinding.ActivityMainBinding
