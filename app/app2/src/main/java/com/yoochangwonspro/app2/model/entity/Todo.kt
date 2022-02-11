b016b1f577cde6e1a271538a08f4c7fbbeb81504
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-02-11 23:22:56 +0900

    Feat : Todo data class Entity 추가

---
+++ app/app2/src/main/java/com/yoochangwonspro/app2/model/entity/Todo.kt
@@ -0,0 +1,14 @@
+package com.yoochangwonspro.app2.model.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo")
+data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+
+    @ColumnInfo(name = "task")
+    val task: String
+)
